951c0bd53253f2dc1d657c206a4a963fec8fe923
Stanley Lai <stanley.lai@telus.com>
2023-01-10 16:09:33 -0800

    import axios, Requesting the Days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,30 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment/index";
+
 import "components/Application.scss";
 
-const days = [
-  {
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
+  const daysApi = "http://localhost:8001/api/days"
+
+  useEffect(() => {
+    axios.get(daysApi)
+    .then(response => {setDays([...response.data])  
+    })
+  }, []);
 
 
   return (
@@ -50,7 +102,17 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => {
+          return (
+            <Appointment
+              key={appointment.id}
+              id={appointment.id}
+              time={appointment.time}
+              interview={appointment.interview}
+            />
+          )})
+        }
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
