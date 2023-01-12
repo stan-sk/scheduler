c7bfde200ad9d34728611f2ac63565e4ca593736
Stanley Lai <stanley.lai@telus.com>
2023-01-11 22:36:58 -0800

    add back function to onCancel in Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -41,7 +41,7 @@ export default function Appointment(props) {
       {mode === CREATE && <Form
         interviewers={[]} 
         onSave={()=>{}}
-        onCancel={()=>{}}
+        onCancel={()=>{back()}}
       />}
     </article>
   );
